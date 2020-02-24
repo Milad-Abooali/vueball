15f854a7fd1f80369034044ad7bad033050f8251
Milad Abooali <m.abooali@hotmail.com>
2020-02-24 13:33:32 +0330

    ed

--- js/main.js
+++ js/main.js
@@ -3,14 +3,28 @@
 
 
 Vue.component('ball', {
-  props: ['ball'],
-  template: `<figure :class="'circle '+ball.color"></figure>`
+  props:['ball'],
+  template: `<figure :class="'circle '+ball.color" v-bind:style="{top: ball.top, left: ball.left}"></figure>`
 })
+
 
 var app = new Vue({
   el: '#app',
   data: {
-    seen: true,
-    creatBall: {text: 'Ledspt',color: 'blue'}
+    test:'ok',
+    balls: [
+        {
+              seen:true,
+              color:'red',
+              top:10,
+              left:10
+        },
+        {
+              seen:true,
+              color:'blue',
+              top:30,
+              left:30
+        }
+      ]
   }
 })
